891a8596c7c6f8e5731d45fe76db69617dd4c63d
Ekaterina <ek.muravyova@gmail.com>
2018-04-15 19:22:52 +0400

    Added

---
+++ WebApplication/Controllers/ValuesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public object GetValues(string key, string value, string add)
+        {
+            return new { key, value, add};
+        }
+    }
+}
